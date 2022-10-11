89f91765ec02d3b2dc66f5337e8c905db76cf4cf
EdwMacias <edwardavid26@hotmail.com>
2022-10-11 09:13:17 -0500

    Se agrega la segunda pagina donde se espera acomodar la interfaz de usuario, pendiente de revision

---
+++ Disenio web/script.js
@@ -0,0 +1,12 @@
+const menudesplegable = document.querySelector(".menu_desplegable");
+const navMenu = document.querySelector(".menu");
+
+menudesplegable.addEventListener("click", () => {
+    menudesplegable.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".link").forEach(n => n.addEventListener("click", () => {
+    menudesplegable.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
